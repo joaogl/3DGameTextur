b01489355ba7500aa5968754c27fa84bb3c74a53
joaogl <joaoguerralourenco@hotmail.com>
2013-03-26 01:23:48 +0000

    COPYRIGHT fix

--- src/com/game/engine/binarydatatree/IBDTNodeReader.java
+++
@@ -1,5 +0,0 @@
-package com.game.engine.binarydatatree;
-
-public interface IBDTNodeReader {
-	public abstract BDTNode readNode() throws Exception;
-}
--- src/com/game/engine/binarydatatree/IBDTNodeWriter.java
+++
@@ -1,7 +0,0 @@
-package com.game.engine.binarydatatree;
-
-import java.io.IOException;
-
-public interface IBDTNodeWriter {
-	public abstract void writeNode(BDTNode node) throws IOException;
-}
